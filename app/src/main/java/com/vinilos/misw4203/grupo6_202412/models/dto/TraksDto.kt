eb0c47a1b3d186fe574452c0e24ec9b16a9202b8
Andres Clavijo Rodriguez <a.clavijo1@uniandes.edu.co>
2024-04-21 16:59:49 -0500

    feat: ✨ Se realiza llamado de api se hacen navegacion de detalle de album y se hace maquetacion de la pantalla de albumes

---
+++ app/src/main/java/com/vinilos/misw4203/grupo6_202412/models/dto/TraksDto.kt
@@ -0,0 +1,4 @@
+package com.vinilos.misw4203.grupo6_202412.models.dto
+
+class TraksDto {
+}
---
+++ app/src/main/java/com/vinilos/misw4203/grupo6_202412/models/service/IAlbumEndpoint.kt
@@ -0,0 +1,4 @@
+package com.vinilos.misw4203.grupo6_202412.models.service
+
+interface IAlbumEndpoint {
+}
