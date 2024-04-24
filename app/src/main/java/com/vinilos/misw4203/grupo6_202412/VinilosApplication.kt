4f6ab6b4775975bf51b46b78baade4129332623a
andresalarcon <w.alarconc@uniandes.edu.co>
2024-04-24 18:41:03 -0500

    Se cambia URL del backend

--- app/src/main/java/com/vinilos/misw4203/grupo6_202412/VinilosApplication.kt
+++ app/src/main/java/com/vinilos/misw4203/grupo6_202412/VinilosApplication.kt
@@ -6,7 +6,7 @@ import com.vinilos.misw4203.grupo6_202412.models.service.VinilosService
 
 class VinilosApplication: Application() {
 
-    private val BASE_URL_API_SERVICE: String = "http://10.0.2.2:3000/"
+    private val BASE_URL_API_SERVICE: String = "http://vynils-back-heroku.herokuapp.com/"
 
     val vinilosService by lazy { VinilosService.create(BASE_URL_API_SERVICE) }
     val vinilosRepository by lazy { VinilosRepository.create(vinilosService) }
