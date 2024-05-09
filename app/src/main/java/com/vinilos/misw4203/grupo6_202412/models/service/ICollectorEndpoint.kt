b7c16a8466e2feefaeb937e05f6430b66f933245
Jairo Bernal <ji.bernal27@uniandes.edu.co>
2024-05-08 23:17:03 -0500

    feat: ✨ Se realiza implementacion de la HU06 detalle de coleccionista

--- app/src/main/java/com/vinilos/misw4203/grupo6_202412/models/service/ICollectorEndpoint.kt
+++ app/src/main/java/com/vinilos/misw4203/grupo6_202412/models/service/ICollectorEndpoint.kt
@@ -1,10 +1,18 @@
 package com.vinilos.misw4203.grupo6_202412.models.service
 
+import com.vinilos.misw4203.grupo6_202412.models.dto.CollectorAlbumDetailDto
 import com.vinilos.misw4203.grupo6_202412.models.dto.CollectorDto
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface ICollectorEndpoint {
     @GET("collectors")
     fun getCollectorsList(): Call<ArrayList<CollectorDto>>;
+
+    @GET("collectors/{id}")
+    fun getCollectorById(@Path("id") id: Int): Call<CollectorDto>;
+
+    @GET("collectors/{id}/albums")
+    fun getCollectorAlbumsById(@Path("id") id: Int): Call<ArrayList<CollectorAlbumDetailDto>>;
 }
